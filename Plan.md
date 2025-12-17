64bfb404aa4fdde2b2c436dd239964ad0853d4c8
Raed Amin <102492020+ChungusEatsFungus@users.noreply.github.com>
2025-12-17 13:17:29 -0500

    Got a lot done. Implemented a nested structure, and started working on the calculations and getting ready to post the results.

--- Plan.md
+++ Plan.md
@@ -1,4 +1,6 @@
+-----------------------------------------------------------
 12-15-2025, 5:34 PM
+
 The application that I am being asked to build does not seem that far-fetched from the weather app I built previously. The only difference is that I am given a single URL to take data from and don't need to build one for each use case, and that I will have to POST the results back after the code is executed.
 
 Time complexity is not a factor, so I don't have to worry too much about that. 
@@ -8,6 +10,27 @@ I will make this project in Python, since that is my most fluent language right 
 I will have to make use of the requests library to GET and POST the data.
 -----------------------------------------------------------
 12-15-2025, 6:02 PM
-I made my first commit to my repository earlier around 5:50 PM.
 
 It seems like it won't be as similar to my weather app as I thought. I will probably have to make different constructs for each type of data (participant info, sessions, rounds). At least, this is what I am thinking of right now.
+-----------------------------------------------------------
+12-15-2025, 7:10 PM
+
+I am completely sure that I have to create different constructs for each type of data now. However, I also feel like it might be a bit easier to also nest each data type within each other as to access the data much more easily at the end of all of this.
+
+The data seems to follow a sort of hierarchy: Round->Session->Participant. So, for each higher hierarchal level class, it should probably contain a list of the lower level objects right below it, I think so at least. I will give this a try.
+-----------------------------------------------------------
+12-17-2025, 11:51 AM
+
+Coming back off of a little break to avoid overworking myself. I realize that a big problem that has been recurring here is the organization of the entire code itself. I need to be making more use of the main() function clearly, and I don't think having a global raw_data variable would work for all of the classes. Maybe I should compartmentalize the data. 
+
+Maybe I should also make another function for processing the data. I did something like that in my weather app I believe, through the display_weather() function. Helper functions might be useful here. 
+-----------------------------------------------------------
+12-17-2025, 12:34 PM
+
+Now I started to expand upon the idea I had two days ago about nesting the classes within each other, under the nested structure all_participants. I was going to make all_participants a dictionary like I did for all_sessions and all_rounds, but maybe I shouldn't because I am going to have to post my solution as a JSON array anyways? Maybe I should store the participants in a list, without any keys linked (such as their participant IDs).
+
+Ah, and it might be easier to sort a list alphabetically than a dictionary anyways, so maybe I should have it put in a list after all. 
+-----------------------------------------------------------
+12-17-2025, 12:43 PM
+
+Yeah, now I get it. The purpose of having the all_rounds and all_sessions be dictionaries is just so the program can grab the items by ID quickly for linking them together. That is no longer necessary by the time we get to the all_participants list, since now the structure is essentially being packaged. It is fully nested by that point. 
--- challenge.py
+++ challenge.py
@@ -24,39 +24,105 @@ def get_data() -> Any:
         print(f"Error: {response.status_code}")
     return None
 
-raw_data: Optional[Dict[str, Any]] = get_data()
+class Round:
+    # Initializing the statistics for the Round data type.
+    def __init__(self, round_data: Dict[str, Any]) -> None:
+        self.roundId: int = round_data['roundId']
+        self.sessionId: int = round_data['sessionId']
+        self.score: int = round_data['score']
+        self.startTime: int = round_data['score']
+        self.endTime: int = round_data['endTime']
 
-class Participant:
-    # Initializing the statistics for the Participant data type.
-    def __init__(self) -> None:
-        if raw_data is not None:
-            self.participantId: int = raw_data['participantInfo']['participantId']
-            self.name: str = raw_data['participantInfo']['name']
-            self.age: int = raw_data['participantInfo']['age']
-            self.sessions: list[int] = raw_data['participantInfo']['sessions']
+    def get_duration(self) -> int:
+        return (self.endTime - self.startTime)
 
 class Session:
     # Initializing the statistics for the Session data type.
-    def __init__(self) -> None:
-        if raw_data is not None:
-            self.participantId: int = raw_data['sessions']['participantId']
-            self.sessionId: int = raw_data['sessions']['sessionId']
-            self.language: str = raw_data['sessions']['language']
-            self.rounds: list[int] = raw_data['sessions']['rounds']
-            self.startTime: int = raw_data['sessions']['startTime']
-            self.endTime: int = raw_data['sessions']['endTime']
+    def __init__(self, session_data: Dict[str, Any]) -> None:
+        self.participantId: int = session_data['participantId']
+        self.sessionId: int = session_data['sessionId']
+        self.language: str = session_data['language']
+        self.startTime: int = session_data['startTime']
+        self.endTime: int = session_data['endTime']
 
-class Round:
-    # Initializing the statistics for the Round data type.
-    def __init__(self) -> None:
-        if raw_data is not None:
-            self.roundId: int = raw_data['rounds']['roundId']
-            self.sessionId: int = raw_data['rounds']['sessionId']
-            self.score: int = raw_data['rounds']['score']
-            self.startTime: int = raw_data['rounds']['score']
-            self.endTime: int = raw_data['rounds']['endTime']
+        self.rounds: list[Round] = []
+
+    def get_duration(self) -> int:
+        return (self.endTime - self.startTime)
+
+    def get_average_score(self) -> float:
+        # Need an accumulator to calculate the average of the total score of all rounds
+        total_score = 0
+        for r in self.rounds:
+            total_score = total_score + r.score
+        average_score = total_score / len(self.rounds)
+        return average_score
+
+class Participant:
+    # Initializing the statistics for the Participant data type.
+    def __init__(self, participant_data: Dict[str, Any]) -> None:
+        self.participantId: int = participant_data['participantId']
+        self.name: str = participant_data['name']
+        self.age: int = participant_data['age']
+
+        self.sessions: list[Session] = []
+
+    def get_participant_stats(self) -> Dict[str, Any]:
+        return {} # Must implement later
 
 def main() -> None:
     """
     The main function -- this where the program will actually start.
     """
+    raw_data: Optional[Dict[str, Any]] = get_data()
+
+    if raw_data is None:
+        print("Failed to retrieve data.")
+        return
+
+    all_rounds: Dict[int, Round] = {} # Initializing a helper dictionary for rounds
+
+    # The following loops are responsible for nesting the objects within each other, and to make all_participants a fully nested structure.
+    # Loop for initializing an all_rounds dict to prepare for linking into session data
+    for r in raw_data['rounds']:
+        new_round = Round(r)
+        all_rounds[new_round.roundId] = new_round
+
+    all_sessions: Dict[int, Session] = {} # Initializing a helper dictionary for sessions
+
+    # Loop for building an all_sessions dict as well as linking round data into session data
+    for s in raw_data['sessions']:
+        new_session = Session(s)
+
+        round_ids = s['rounds']
+
+        for round_id in round_ids:
+            if round_id in all_rounds:
+                selected_round = all_rounds[round_id]
+                new_session.rounds.append(selected_round)
+
+        all_sessions[new_session.sessionId] = new_session
+
+    all_participants: list[Participant] = [] # Initializing a final list for participants
+
+    # Loop for building an all_participants list as well as linking session data into participant data
+    for p in raw_data['participantInfo']:
+        new_participant = Participant(p)
+
+        session_ids = p['sessions']
+
+        for session_id in session_ids:
+            if session_id in all_sessions:
+                selected_session = all_sessions[session_id]
+                new_participant.sessions.append(selected_session)
+
+        all_participants.append(new_participant)
+
+    final_output: list[Dict[str, Any]] = []
+
+    for participant in all_participants:
+        stats = participant.get_participant_stats()
+        final_output.append(stats)
+
+if __name__ == "__main__":
+    main()
